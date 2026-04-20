c676bc9c27695b22a94534dbbc961f2bfa34802b
Long Dap Chai <longnh.tech00@gmail.com>
2026-04-20 14:21:51 +0700

    fix: fix version to latest

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,10 @@ const { fontFamily } = require("tailwindcss/defaultTheme")
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class"],
-  content: ["app/**/*.{ts,tsx}", "components/**/*.{ts,tsx}"],
+  content: ["src/**/*.{ts,tsx}"],
+  corePlugins: {
+    preflight: false,
+  },
   theme: {
     container: {
       center: true,
